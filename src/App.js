87f4201ef5d6f92bc3c6153070411e9dda1ef599
Laura Heino <lhhein@gmail.com>
2021-03-10 14:21:30 +0100

    more UI fixes

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ class App extends Component {
     return (
 
       <div>
-      <NavBar onLogout={this.handleLogout} user={loggedInUser}/>
+      { loggedInUser && <NavBar onLogout={this.handleLogout} user={loggedInUser}/> }
 
       <Switch>
 
